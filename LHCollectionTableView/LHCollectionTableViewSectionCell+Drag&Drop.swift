c67e570cf0bbda927f010ea677973def23b94ebb
Li-Heng Hsu <yesleon@me.com>
2018-11-09 14:48:31 +0800

    Refactor

--- LHCollectionTableView/LHCollectionTableViewSectionCell+Drag&Drop.swift
+++ LHCollectionTableView/LHCollectionTableViewSectionCell+Drag&Drop.swift
@@ -24,21 +24,10 @@ protocol LHCollectionTableViewSectionCellDropDelegate: AnyObject {
 extension LHCollectionTableViewSectionCell: UICollectionViewDragDelegate {
     
     public func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
-//        guard dataSource?.sectionCell(self, canMoveItemAt: indexPath) == true else { return [] }
-//        if let section = (superview as? UITableView)?.indexPath(for: self)?.row {
-//            let item = UIDragItem(itemProvider: NSItemProvider())
-//            item.localObject = IndexPath(item: indexPath.item, section: section)
-//            return [item]
-//        }
-//        return []
         return dragDelegate?.sectionCell(self, itemsForBeginning: session, at: indexPath) ?? []
     }
     
     public func collectionView(_ collectionView: UICollectionView, dragPreviewParametersForItemAt indexPath: IndexPath) -> UIDragPreviewParameters? {
-//        let preview = UIDragPreviewParameters()
-//        let cell = collectionView.cellForItem(at: indexPath)!
-//        preview.visiblePath = UIBezierPath(roundedRect: cell.bounds, cornerRadius: 5)
-//        return preview
         return dragDelegate?.sectionCell(self, dragPreviewParametersForItemAt: indexPath)
     }
     
@@ -47,31 +36,10 @@ extension LHCollectionTableViewSectionCell: UICollectionViewDragDelegate {
 extension LHCollectionTableViewSectionCell: UICollectionViewDropDelegate {
     
     public func collectionView(_ collectionView: UICollectionView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UICollectionViewDropProposal {
-//        if let dragSession = session.localDragSession,
-//            let item = dragSession.items.first,
-//            item.localObject is IndexPath {
-//            return UICollectionViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
-//        }
-//        return UICollectionViewDropProposal(operation: .cancel)
         return dropDelegate?.sectionCell(self, dropSessionDidUpdate: session, withDestinationIndexPath: destinationIndexPath) ?? UICollectionViewDropProposal(operation: .cancel)
     }
     
     public func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
-//        guard let dropItem = coordinator.items.first else { return }
-//        let itemCount = collectionView.numberOfItems(inSection: 0)
-//        var destinationIndexPath = coordinator.destinationIndexPath ?? IndexPath(item: itemCount, section: 0)
-//        guard let section = (superview as? UITableView)?.indexPath(for: self)?.row else { return }
-//        destinationIndexPath.section = section
-//        let item = dropItem.dragItem
-//        if let sourceIndexPath = item.localObject as? IndexPath {
-//            if sourceIndexPath.section == destinationIndexPath.section,
-//                destinationIndexPath.item == itemCount {
-//                destinationIndexPath.item -= 1
-//            }
-//            dataSource?.sectionCell(self, moveItemAt: sourceIndexPath, toIndexPath: destinationIndexPath)
-//            destinationIndexPath.section = 0
-//            coordinator.drop(item, toItemAt: destinationIndexPath)
-//        }
         dropDelegate?.sectionCell(self, performDropWith: coordinator)
     }
     
